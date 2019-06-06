e938c26009151c09bf4907f2bcce9039cb00caa0
James Hiegel <james.hiegel@gmail.com>
2019-06-06 16:44:11 -0700

    Update install_netcorehosting.ps1

--- install_netcorehosting.ps1
+++ install_netcorehosting.ps1
@@ -1,5 +1,5 @@
 Add-WindowsFeature Web-Server, Web-Basic-Auth, Web-Windows-Auth, Web-Mgmt-Console
--NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1'))) -InstallDir 'C:\Program Files\dotnet'"
+Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1' -InstallDir 'C:\Program Files\dotnet'"
 net stop was /y
 net start w3svc
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
