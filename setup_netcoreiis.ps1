9e6e6b67a9d56b9daa85db824b7a6f68d4574a41
James Hiegel <james.hiegel@gmail.com>
2019-06-06 19:13:02 -0700

    Update setup_netcoreiis.ps1

--- setup_netcoreiis.ps1
+++ setup_netcoreiis.ps1
@@ -1,8 +1,8 @@
 Add-WindowsFeature Web-Server, Web-Basic-Auth, Web-Windows-Auth, Web-Mgmt-Console
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1'))) -InstallDir 'C:\Program Files\dotnet'"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1'))) -InstallDir 'C:\Program Files\dotnet'
 net stop was /y
 net start w3svc
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 cinst -y --params="Quiet IgnoreMissingIIS" dotnetcore-windowshosting
 $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
 $newpath = "$oldpath;C:\Program Files\dotnet"
