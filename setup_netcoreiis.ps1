8ee3d4b44453e11ee149c91c82fcfd04bc2d6bf5
James Hiegel <james.hiegel@gmail.com>
2019-06-06 19:40:19 -0700

    Update setup_netcoreiis.ps1

--- setup_netcoreiis.ps1
+++ setup_netcoreiis.ps1
@@ -1,7 +1,5 @@
 Add-WindowsFeature Web-Server, Web-Basic-Auth, Web-Windows-Auth, Web-Mgmt-Console
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1'))) -InstallDir 'C:\Program Files\dotnet'
-net stop was /y
-net start w3svc
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 cinst -y --params="Quiet IgnoreMissingIIS" dotnetcore-windowshosting
 $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
